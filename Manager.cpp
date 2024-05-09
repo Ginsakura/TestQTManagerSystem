00ae03b2d8679e825b15f45839e137dda53a6856
sraremetal <1850840617@qq.com>
2024-05-09 19:44:37 +0800

    update sql

--- Manager.cpp
+++ Manager.cpp
@@ -9,10 +9,22 @@ Manager::Manager(QWidget *parent)
     db.setDatabaseName("Manager");
     db.setUserName("admin");
     db.setPassword("123456");
+    query = QSqlQuery(db);
     bool sql_open=db.open();
     if(!sql_open){
         qDebug()<<"打开失败";
     }
+        //创建roomstatu sql
+        QString create_table="create table if not exists RoomStatu(RoomNumber int primary key,Reservation bool,ReservationDate datetime, CheckIn bool,CheckInDate datetime,CheckInTime datetime,PeopleNumber int not null,Remark text)";
+        bool create=query.exec(create_table);
+        if(!create){
+            qDebug()<<"创建失败"<<query.lastError().text();
+        }else{
+            qDebug()<<"创建成功";
+        }
+        query.exec("create table if not exists Event(DateTime datetime primary key,RoomNumber int not null,Event text not null,state int ) ");
+        query.exec("create table if not exists Room(RoomNumber int primary key,Price int not null,VIP int)");
+        query.exec("create table if not exists Roomer(DateTime datetime primary key,Name text not null,PersonID text(18) not null,Phone text(11) not null,Gender bool not null,RoomNumber int not null,UseVIP bool ,State int )");
     db.close();
 }
 
