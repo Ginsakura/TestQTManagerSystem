e2b5911c325c26978d4e141109e81708f7e3f3c2
Ginsakura <ai.0121.000.256@gmail.com>
2024-05-10 11:36:09 +0800

    完善SQL封装

--- Manager.cpp
+++ Manager.cpp
@@ -5,30 +5,8 @@ Manager::Manager(QWidget *parent)
 {
     ui.setupUi(this);
     ui.widget->setWindowTitle("酒店前台管理系统");
-    db = QSqlDatabase::addDatabase("QSQLITE");
-    db.setHostName("127.0.0.1");
-    db.setDatabaseName("Manager.db");
-    db.setUserName("admin");
-    db.setPassword("123456");
-    query = QSqlQuery(db);
-    bool sql_open=db.open();
-    if(!sql_open){
-        qDebug()<<"打开失败";
-    }
-        //创建roomstatu sql
-        QString create_table="create table if not exists RoomStatu(RoomNumber int primary key,Reservation bool,ReservationDate datetime, CheckIn bool,CheckInDate datetime,CheckInTime datetime,PeopleNumber int not null,Remark text)";
-        bool create=query.exec(create_table);
-        if(!create){
-            qDebug()<<"创建失败"<<query.lastError().text();
-        }else{
-            qDebug()<<"创建成功";
-        }
-        query.exec("create table if not exists Event(DateTime datetime primary key,RoomNumber int not null,Event text not null,state int ) ");
-        query.exec("create table if not exists Room(RoomNumber int primary key,Price int not null,VIP int)");
-        query.exec("create table if not exists Roomer(DateTime datetime primary key,Name text not null,PersonID text(18) not null,Phone text(11) not null,Gender bool not null,RoomNumber int not null,UseVIP bool ,State int )");
-//        query.exec("create table if not exists Account(account ) ");
-        //设置stackedwidget初始页面
-        ui.stackedWidget->setCurrentWidget(ui.page_2);
+    //设置stackedwidget初始页面
+    ui.stackedWidget->setCurrentWidget(ui.page_2);
     db.close();
 }
 
--- sql.h
+++ sql.h
@@ -13,21 +13,105 @@ class SQLOperat
 {
 public:
 	SQLOperat(QSqlDatabase);
+	SQLOperat();
 	~SQLOperat();
-	QListData* getAll(QString);
+	QSqlQuery getAll(QString);
+	QSqlQuery Select(QString, QString, QString);
+	bool Insert(QString, QString, QString);
+	bool Update(QString, QString, QString, QString);
 
 private:
-	const QSqlDatabase DB;
+	QSqlDatabase DB;
 	QSqlQuery cursor;
 };
 
-SQLOperat::SQLOperat(QSqlDatabase db) :DB(db) {
+SQLOperat::SQLOperat(QSqlDatabase db) {
+	DB = db;
 	cursor = QSqlQuery(DB);
 }
-QListData* SQLOperat::getAll(QString table) {
+SQLOperat::SQLOperat() {
+	DB = QSqlDatabase::addDatabase("QSQLITE");
+	DB.setHostName("127.0.0.1");
+	DB.setDatabaseName("Manager.db");
+	DB.setUserName("admin");
+	DB.setPassword("123456");
+	if (!DB.open()) {
+		qout << QFgColor(0xff, 0, 0) << "打开失败" << QResetColor();
+	}
+	cursor = QSqlQuery(DB);
+	//create table
+	cursor.prepare("create table if not exists RoomStatu ("
+		"RoomNumber int primary key,"
+		"Reservation bool,"
+		"ReservationDate datetime,"
+		"CheckIn bool,"
+		"CheckInDate datetime,"
+		"CheckInTime datetime,"
+		"PeopleNumber int not null,"
+		"Remark text )");
+	cursor.prepare("create table if not exists Event ("
+		"DateTime datetime primary key,"
+		"RoomNumber int not null,"
+		"Event text not null,"
+		"state int );");
+	cursor.prepare("create table if not exists Room ("
+		"RoomNumber int primary key,"
+		"Price int not null,"
+		"VIP int)");
+	cursor.prepare("create table if not exists Roomer ("
+		"DateTime datetime primary key,"
+		"Name text not null,"
+		"PersonID text(18) not null,"
+		"Phone text(11) not null,"
+		"Gender bool not null,"
+		"RoomNumber int not null,"
+		"UseVIP bool,"
+		"State int);");
+	cursor.prepare("create table if not exists User ("
+		"UserName text primary key,"
+		"Password not null,"
+		"Permission bool default false);");
+	bool create = cursor.execBatch();
+	if (create) {
+		qout << QFgColor(0, 0xff, 0) << "创建成功" << QResetColor();
+	}
+	else {
+		qout << QFgColor(0xff, 0, 0) << "创建失败" << QResetColor() << cursor.lastError().text();
+	}
+}
+QSqlQuery SQLOperat::getAll(const QString table) {
 	cursor.prepare(QString("SELECT * from %1").arg(table));
 	if (!cursor.exec()) {
 		qout << QFgColor(0xff, 0, 0) << "select error: " << QResetColor() << cursor.lastError();
+	}
+	else {
+		return cursor;
+	}
+}
+bool SQLOperat::Insert(const QString table, const QString keys, const QString value) {
+	cursor.prepare(QString("Insert into %1(%2) values (%3)").arg(table).arg(keys).arg(value));
+	bool state = cursor.exec();
+	if (!state) {
+		qout << QFgColor(0xff, 0, 0) << "Insert to" << table << " error: " << QResetColor() << cursor.lastError();
+	}
+	return state;
+}
+bool SQLOperat::Update(const QString table, const QString keys, const QString value, const QString condition) {
+	cursor.prepare(QString("Update %1 set '%2'='%3' where %3").arg(table).arg(keys).arg(value).arg(condition));
+	bool state = cursor.exec();
+	if (!state) {
+		qout << QFgColor(0xff, 0, 0) << "Insert to" << table << " error: " << QResetColor() << cursor.lastError();
+	}
+	return state;
+}
+QSqlQuery SQLOperat::Select(const QString table, const QString getter, const QString condition) {
+	cursor.prepare(QString("select %1 from %2 where %3").arg(getter).arg(table).arg(condition));
+	bool state = cursor.exec();
+	if (!cursor.exec()) {
+		qout << QFgColor(0xff, 0, 0) << "select error: " << QResetColor() << cursor.lastError();
+	}
+	else {
+		return cursor;
 	}
 }
 
